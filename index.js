85113117a2445b29f2c2d29c942c7761d49d198e
pranav <pranav_a@ece.iitr.ac.in>
2021-08-20 12:11:19 +0530

    Hire Me added

---
+++ index.js
@@ -0,0 +1,6 @@
+function sendEmail() {
+    var email = "pranavleo22@gmail.com";
+    var subject = "I want to hire you for ";
+    
+    document.location = "mailto:"+email+"?subject="+subject ;
+}
